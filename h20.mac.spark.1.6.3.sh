980cb87630d996238488c53ce807ea026de5b693
snewhouse <stephen.j.newhouse@gmail.com>
2016-06-10 16:32:18 +0100

    spark stuff 1

--- h20.mac.spark.1.6.3.sh
+++ h20.mac.spark.1.6.3.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
 
-## move to HOME
-cd ${INSTALL_DIR}
+## SPARK ------------------------------------------##
 
-## get spark and stick it in  /usr/local/
-cd /usr/local
+## get spark and stick it in /usr/local/
 curl -OL http://mirror.catn.com/pub/apache/spark/spark-1.6.1/spark-1.6.1-bin-hadoop2.6.tgz | tar -xz -C /usr/local/
 
+## add symbolic link
 cd /usr/local && \
 ln -s spark-1.6.1-bin-hadoop2.6 spark
 
+## sort permissions
 chmod -R 755 spark
 
+## export spark path
 export SPARK_HOME="/usr/local/spark"
 
+## add to my
 echo 'export SPARK_HOME="/usr/local/spark"' >> ~/.zshrc ## specific to my local mac
-
 source ~/.zshrc
 
+# echo 'export SPARK_HOME="/usr/local/spark"' >> ~/.bashrc ## specific to my local mac
+# source ~/.bashrc
 
 ## SET JAVA_HOME
+#  /usr/libexec/java_home  -V
+export JAVA_HOME="/Library/Java/Home"
+echo 'export JAVA_HOME="/Library/Java/Home"' >> ~/.zshrc ## specific to my local mac
+source ~/.zshrc
 
+## H20 ------------------------------------------#
 ## get h20
 
+
+## sparking water -------------------------------#
 ## get sparking water
 wget http://h2o-release.s3.amazonaws.com/sparkling-water/rel-1.6/3/sparkling-water-1.6.3.zip
