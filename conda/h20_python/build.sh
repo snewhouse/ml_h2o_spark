58a839024753715e72203e187694f0b2f4e78ac5
snewhouse <stephen.j.newhouse@gmail.com>
2016-06-13 16:13:39 +0100

    testing

--- conda/h20_python/build.sh
+++ conda/h20_python/build.sh
@@ -7,5 +7,5 @@ pip install -U tabulate
 pip install -U future
 pip install -U six
 pip install scikit-learn
-pip uninstall h2o
-pip install http://h2o-release.s3.amazonaws.com/h2o/rel-turchin/8/Python/h2o-3.8.2.8-py2.py3-none-any.whl
+##pip uninstall h2o
+pip install h2o
